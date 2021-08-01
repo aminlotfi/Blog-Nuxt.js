33642c42ebdd5a594cfaf95667c1114371c04749
AminLotfi <a.lotfi1201@gmail.com>
2021-08-01 12:46:08 +0430

    Store data in Firebase.

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="submitForm"/>
     </section>
   </div>
 </template>
 
 <script>
 import AdminPostForm from "@/components/Admin/AdminPostForm";
+import axios from 'axios'
 
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
+  },
+  methods: {
+    submitForm(postData) {
+      axios.post('https://nuxt-blog-3d4dd-default-rtdb.firebaseio.com/posts.json', postData)
+      .then(result => console.log(result))
+      .catch(e => console.log(e));
+    }
   }
 };
 </script>
